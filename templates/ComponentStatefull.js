458a2a70a7a095ea33332628532a87daaf83b728
flfk <flfk@users.noreply.github.com>
2018-08-17 22:15:31 -0300

    Added component templates

---
+++ templates/ComponentStatefull.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const propTypes = {};
+
+const defaultProps = {};
+
+class XX extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default XX;
---
+++ templates/ComponentStateless.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+const propTypes = {};
+
+const defaultProps = {};
+
+const XX = props => {
+  return <div />;
+};
+
+export default XX;
+
+import styled from "styled-components";
