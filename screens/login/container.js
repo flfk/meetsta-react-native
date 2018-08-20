b617c41aaab56339a05de52bdc666b9bb1eceb4c
flfk <flfk@users.noreply.github.com>
2018-08-20 18:02:37 -0300

    Adding shared container component

--- screens/login/container.js
+++ screens/login/container.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import { Constants } from "expo";
 
 const Container = styled.View`
+  flex: 1;
+  align-items: center;
   justify-content: center;
+  padding-top: ${Constants.statusBarHeight};
 `;
 
 export default Container;
