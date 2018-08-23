0265b7fa99f80f6abf6288fd0facf33ceee1ea6b
flfk <flfk@users.noreply.github.com>
2018-08-23 18:53:17 -0300

    Minor changes

--- screens/AddEvent/index.js
+++ screens/AddEvent/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { View, Text, Button } from "react-native";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, Button } from 'react-native';
 
-import Container from "../../shared/Container";
+import Container from '../../shared/Container';
 
 const propTypes = {};
 
